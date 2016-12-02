7d17c8671c48229b17aa4ee26d43510e635e5f05
v-liuson <v-liuson@MSDNUWPSERVER1>
2016-12-02 15:30:51 +0800

    11

--- XamarinCustomContentPage/XamarinCustomContentPage/CustomPage.cs
+++ XamarinCustomContentPage/XamarinCustomContentPage/CustomPage.cs
@@ -13,8 +13,11 @@ namespace XamarinCustomContentPage
         {
             this.ButtonClicked = (ob, arg) => { Navigation.PushAsync(new CustomPage()); };
         }
+
+        //Expose a event to add behaviors
         public event EventHandler ButtonClicked;
 
+        //call this method in corresponding renders and using the register event
         public void NotifyButtonClicked()
         {
             if (ButtonClicked != null)
